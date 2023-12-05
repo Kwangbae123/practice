fcf631fff224605303187a370cb365ae6324df8f
Kwangbae123 <sungjinhan@kakao.com>
2023-12-05 17:10:28 +0900

    revise 1

--- chapter4.py
+++ chapter4.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys, os
 sys.path.append(os.pardir)
 from mnist import load_mnist
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
 #오차제곱합(sum of squares for error, SSE)
 def sum_squares_error(y, t):
@@ -15,6 +15,9 @@ def cross_entropy_error(y, t):
     if y.ndim == 1: # y가 1차원이라면
         t = t.reshape(1, t.size) # t는 정답 레이블 reshape함수로 형상을 바꿔준다.
         y = y.reshape(1, y.size) # y는 신경망의 출력
+
+    if t.size == y.size: # 훈련데이터가 원-핫-인코딩 벡터라면 label의 인덱스로 변환
+        t = t.argmax(axis=1)
 
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size # 정답 label이 '2', '7' 숫자 일때
@@ -60,17 +63,57 @@ def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
 
-    for idx in range(x.size):
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx] # f(x+h) 계산
         x[idx] = tmp_val +h
         fxh1 = f(x)
 
-        x[idx] = tmp_val - h
+        x[idx] = tmp_val - h # f(x-h) 계산
         fxh2 = f(x)
 
         grad[idx] = (fxh1 - fxh2) / (2*h)
         x[idx] = tmp_val #값 복원
+        it.iternext()
 
     return grad
 
 # print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+
+# gradient descent(경사하강법)
+# f = 최적화 하려는 함수, init_x = 초기값, lr = learning rate, step_num = 반복횟수
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x) # numerical_gradient를 이용해 함수의 기울기를 구한다.
+        x -= lr*grad
+
+from chapter3 import softmax
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3) # 정규분포로 초기화 한다.
+    def predict(self, x): # 예측을 수행하는 메서드
+        return np.dot(x, self.W)
+    def loss(self, x, t): # 손실함수의 값을 구하는 메서드
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+net = simpleNet()
+# print(net.W)
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+# print(p)
+# print(np.argmax(p)) # 최대값의 인덱스
+t = np.array([0, 0, 1]) # 정답레이블
+# print(net.loss(x, t))
+
+def f(W):
+    return net.loss(x, t)
+
+dW = numerical_gradient(f, net.W)
+print(dW)
---
+++ mini_batch_train.py
@@ -0,0 +1,48 @@
+import numpy as np
+from mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+#하이퍼 파라미터
+iters_num = 1000 # 반복횟수
+train_size = x_train.shape[0]
+batch_size = 100 # 미니배치 크기
+learning_rate = 0.1 # 학습률
+
+train_loss_list =[]
+train_acc_list = []
+test_acc_list = []
+
+# 1 epoch당 반복수
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size= 784, hidden_size= 50, output_size= 10)
+
+for i in range(iters_num):
+    # 미니배치 획득
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 기울기 계산
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    #매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    # print(train_loss_list)
+
+    # 1 epoch당 정확도 계산
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train,t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc : {0}, {1}".format(train_acc, test_acc))
+
+
